adde254ec64c9a4fd70d3377b615f91fb0833c17
tyamagis <thiago.yamagishi@capgemini.com>
2023-09-15 12:01:21 -0300

    Initial commit

---
+++ src/main/java/com/projeto/wplex/geolocatefinder/geolocatefinder/GeoLocateFinderApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.wplex.geolocatefinder.geolocatefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoLocateFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoLocateFinderApplication.class, args);
+	}
+
+}
